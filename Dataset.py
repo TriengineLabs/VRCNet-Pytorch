591d34321ee3b4ef326c54f71e47dc278ed8f219
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2019-02-23 18:03:20 +0400

    Added data loading and preprocessing in Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -1,12 +1,21 @@
 from torch.utils.data import Dataset
+import librosa
 
 
 class WaveDataset(Dataset):
-    def __init__(self, dataframe):
+    def __init__(self, dataframe, data_path='Dataset/'):
+        self.data_path = data_path
         self.dataframe = dataframe
 
     def __len__(self):
         return len(self.dataframe)
 
     def __getitem__(self, item):
-        pass
+        paths = self.dataframe.iloc[1, :].values
+        mel_specs = []
+        for p in paths:
+            y, sr = librosa.load(self.data_path+p)
+            mlc = librosa.feature.melspectrogram(y=y, sr=sr)
+            mel_specs.append(mlc)
+
+        return mel_specs
