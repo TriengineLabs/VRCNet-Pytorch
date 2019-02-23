0995c5b4ca8cb48d912e1af136ab8f04f6c9d58a
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2019-02-23 15:47:14 +0400

    Initial Commit with basic Dataset Preprocessing template

---
+++ Dataset.py
@@ -0,0 +1,12 @@
+from torch.utils.data import Dataset
+
+
+class WaveDataset(Dataset):
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, item):
+        pass
