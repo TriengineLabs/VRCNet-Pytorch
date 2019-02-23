ab6306e50b0f875337483082d87b6ec187635592
Karlos <carlosmuradyan2@gmail.com>
2019-02-23 18:35:36 +0400

    Added Deep U-Net model with initial implementation

---
+++ DeepUNet.py
@@ -0,0 +1,36 @@
+import torch.nn.Functional as F
+from .DeepUNet_utils import *
+
+class DeepUNet(nn.Module):
+    def __init__(self, n_channels, n_classes):
+        super(DeepUNet, self).__init__()
+
+        self.enc1 = EncoderLayer(n_channels, 16)
+        self.enc2 = EncoderLayer(16, 32)
+        self.enc3 = EncoderLayer(32, 64)
+        self.enc4 = EncoderLayer(64, 128)
+        self.enc5 = EncoderLayer(128, 256)
+        self.enc6 = EncoderLayer(256, 512)
+        self.dec1 = DecoderLayer(512, 256)
+        self.dec2 = DecoderLayer(512, 128)
+        self.dec3 = DecoderLayer(256, 64)
+        self.dec4 = DecoderLayer(128, 32)
+        self.dec5 = DecoderLayer(64, 16)
+        self.dec6 = DecoderLayer(32, n_classes, concat=False)
+
+    def forward(self, x):
+        x1 = self.enc1(x)
+        x2 = self.enc2(x1)
+        x3 = self.enc3(x2)
+        x4 = self.enc4(x3)
+        x5 = self.enc5(x4)
+        x6 = self.enc6(x5)
+
+        x = self.dec1(x6, x5)
+        x = self.dec2(x, x4)
+        x = self.dec3(x, x3)
+        x = self.dec4(x, x2)
+        x = self.dec5(x, x1)
+        x = self.dec6(x)
+
+        return F.sigmoid(x)
---
+++ DeepUNet_utils.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torch.nn.Functional as F
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, input_channel, output_channel):
+        super(EncoderLayer, self).__init__()
+
+        #TODO change Relu to leaky Relu
+        #TODO check Conv2d output size
+        self.encoding = nn.Sequential(
+                nn.Conv2d(input_channel, output_channel, 5, stride=2),
+                nn.ReLU(inplace=True)
+                )
+
+    def forward(self, x):
+        x = self.encoding(x)
+        return x
+
+class DecoderLayer(nn.Module):
+    def __init__(self, input_channel, output_channel):
+        super(DecoderLayer, self).__init__()
+
+        self.decoding = nn.Sequential(
+                nn.ConvTranspose2d(input_channel, output_channel, 5, stride=2, padding=2),
+                nn.ReLU(inplace=True)
+                )
+
+    def forward(self, x1, x2=None):
+
+        x1 = self.decoding(x1)
+
+        if not x2:
+            return x1
+        else:
+            diffY = x2.size()[2] - x1.size()[2]
+            diffX = x2.size()[3] - x1.size()[3]
+
+            x1 = F.pad(x1, (diffX // 2, diffX - diffX//2,
+                            diffY // 2, diffY - diffY//2))
+
+            x = torch.cat([x2, x1], dim=1)
+
+            return x
