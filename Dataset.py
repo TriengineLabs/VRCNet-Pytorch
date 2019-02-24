236fe847d5ff2abd439deff356abc04c1160d3f0
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2019-02-24 16:41:54 +0400

    Editied Transform feature of Dataset to accept several transforms

--- Dataset.py
+++ Dataset.py
@@ -1,9 +1,5 @@
 from torch.utils.data import Dataset
-from scipy import signal
-from scipy.io import wavfile
 import librosa
-import torch
-import os
 
 
 class WaveDataset(Dataset):
@@ -20,11 +16,11 @@ class WaveDataset(Dataset):
         mel_specs = []
         for i, p in enumerate(paths):
             inp, sr = librosa.load(self.data_path + p)
-            mlc, phase = librosa.magphase(librosa.stft(inp, n_fft=1024,hop_length=256,window='hann',center='True'))
-
-            if i == 0 and self.transforms:
-                mlc_tr = self.transforms(mlc)
-                mel_specs.append(mlc_tr)
+            mlc, phase = librosa.magphase(librosa.stft(inp, n_fft=1024, hop_length=256, window='hann', center=True))
             mel_specs.append(mlc)
+
+        if self.transforms:
+            for tr in self.transforms:
+                mel_specs = tr(mel_specs)
 
         return mel_specs
