8f1a78cfa28198e678d41f48461a2a6b799642d2
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2019-03-24 21:58:08 +0400

    Data is preprocessing edited, multiprocessing added
    - Now each ausio is split into equal parts and all parts are being used
    - resampling is made optional
    - multiprocessing added

--- preprocess.py
+++ preprocess.py
@@ -4,35 +4,65 @@ import musdb
 import h5py
 import librosa
 import pandas as pd
+from transforms import HorizontalCrop
+import parmap
+from icecream import ic
 
-def prepare_dataset(data_path, subset=None, path_to_save='./numpy_data', processed_csv_path='./processed_dataset.csv'):
+def prepare_dataset(data_path, subset=None,
+                    path_to_save='./numpy_data',
+                    processed_csv_path='./processed_dataset.csv',
+                    resample_rate=None,
+                    n_fft=2048,
+                    hop_length=512,
+                    slice_duration=512):
     mus = musdb.DB(root_dir='musdb')
     music_list = mus.load_mus_tracks(subsets='train')
     print('Starting preparing dataset...')
     if not os.path.exists(path_to_save):
         os.makedirs(path_to_save)
     processed_csv = pd.DataFrame(columns=['mix'] + list(music_list[0].targets.keys()))
-    for item in tqdm(range(len(music_list))):
-        row_to_insert = []
+    # p = multiprocessing.Pool(6)
+    rows = parmap.map(process_audio, music_list[:16], processed_csv, pm_pbar=True, pm_processes=4)
+    for r in rows:
+        for n in r:
+            processed_csv.loc[len(processed_csv)] = n
 
-        # paths_of_mix = dataset_csv.iloc[item, :].values
-        audio = music_list[item]
-        for i, p in enumerate(processed_csv.columns):
-            if p == 'mix':
-                inp = librosa.to_mono(audio.audio.transpose())
-            else:
-                inp = librosa.to_mono(audio.targets[p].audio.transpose())
+    processed_csv.to_csv(processed_csv_path, index=False)
 
-            sr = audio.rate
-            inp = librosa.resample(inp, sr, 8192)
-            ft_inp = librosa.stft(inp, n_fft=1024, hop_length=768, window='hann', center=True)
+def process_audio(audio, processed_csv,
+                  path_to_save='./numpy_data',
+                  resample_rate=None,
+                  n_fft=2048,
+                  hop_length=512,
+                  slice_duration=512):
+    rows = []
 
-            filename = audio.name + '.' + p
+    # paths_of_mix = dataset_csv.iloc[item, :].values
+    # audio = music_list[item]
+    for i, p in enumerate(processed_csv.columns):
+        if p == 'mix':
+            inp = librosa.to_mono(audio.audio.transpose())
+        else:
+            inp = librosa.to_mono(audio.targets[p].audio.transpose())
+
+        sr = audio.rate
+        if resample_rate:
+            inp = librosa.resample(inp, sr, resample_rate)
+        ft_inp = librosa.stft(inp, n_fft=n_fft, hop_length=hop_length, window='hann', center=True)
+
+        # print(ft_inp.shape)
+        for tr in range(ft_inp.shape[1] // slice_duration):
+            ft_inp_slice = ft_inp[tr * slice_duration:(tr + 1) * slice_duration]
+            filename = audio.name + '.' + p + '_' + str(tr)
 
             np_file_path = os.path.join(path_to_save, (filename + '.h5'))
             with h5py.File(np_file_path, 'w') as hf:
-                hf.create_dataset('dataset', data=ft_inp)
-            row_to_insert.append(np_file_path)
+                hf.create_dataset('dataset', data=ft_inp_slice)
+            if len(rows) > tr:
+                rows[tr].append(np_file_path)
+            else:
+                rows.append([np_file_path])
 
-        processed_csv.loc[len(processed_csv)] = row_to_insert
-    processed_csv.to_csv(processed_csv_path, index=False)
+    return rows
+        # for n in rows:
+            # processed_csv.loc[len(processed_csv)] = n
